770cc12ffa7eede90b4787de309561fd04245b00
7Funk <danik.pro42@gmail.com>
2022-03-01 19:02:11 +0300

    esskeetit3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
-import FAQ  from "./pages/FAQ/FAQ";
-=======
 import { Benefits } from "./components/Benefits/Benefits";
 import { Header } from "./components/Header/Header";
 import { Main } from "./pages/Main/Main";
 import { Story } from "./pages/Story/Story";
 
->>>>>>> pens
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <FAQ/>
-=======
       <Header />
       <Main />
       <Story />
       <Benefits />
->>>>>>> pens
     </div>
   );
 }
