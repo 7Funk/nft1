6128b0f15666cf8a74a5c078aaa6d4854e926b64
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2022-03-01 18:05:40 +0500

    added wuestion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Question } from "./components/Question/Question";
 
 function App() {
   return (
     <div>
+      <Question answer='lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ' question='text' />
     </div>
   );
 }
