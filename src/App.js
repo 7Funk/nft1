4a6dd1f8e8836114843e6501dc98816db165d22a
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2022-03-02 21:50:01 +0500

    faq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { FAQ } from "./pages/FAQ";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components";
+
+import { FAQ, Main } from "./pages";
 
 const App = () => {
   return (
-    <div>
-      <FAQ />
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </Layout>
   );
-}
+};
 
 export default App;
