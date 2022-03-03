618750faa7e348c882a042f8e9122b59f426ab2c
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2022-03-03 21:20:54 +0500

    updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,15 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <img src="/images/headerLogo.svg" alt="logo" />
-      <a className={styles.Link} href="#story">Story</a>
-      <a className={styles.Link} >The MetaFluencer`s</a>
-      <a className={styles.Link} href="#benefits">Benefits</a>
-      <a className={styles.Link}>Eco</a>
-      <a className={styles.Link}>Team</a>
-      <a className={styles.Link}>Auction</a>
-      <a className={styles.Link}>Contracts</a>
-      <Link className={styles.Link} to="/faq">FAQ</Link>
-      <a className={styles.Link}>Whitepaper</a>
+      <a className={styles.link} href="#story">Story</a>
+      <a className={styles.link} >The MetaFluencer`s</a>
+      <a className={styles.link} href="#benefits">Benefits</a>
+      <a className={styles.link}>Eco</a>
+      <a className={styles.link}>Team</a>
+      <a className={styles.link}>Auction</a>
+      <Link to="/contacts" className={styles.link}>Contacts</Link>
+      <Link className={styles.link} to="/faq">FAQ</Link>
+      <a className={styles.link}>Whitepaper</a>
       <button className={styles.button}>CONNECT WALLET {">"}</button>
     </header>
   );
