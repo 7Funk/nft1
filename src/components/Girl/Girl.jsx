c5b9f158a4b4e37b3c58aea9e3751042ad087992
7Funk <danik.pro42@gmail.com>
2022-03-04 18:42:12 +0300

    esskeetit8

---
+++ src/components/Girl/Girl.jsx
@@ -0,0 +1,21 @@
+import { style } from '@mui/system'
+import React from 'react'
+
+import { Container } from '..'
+import styles from './Girl.module.scss'
+
+const Girl = () => {
+  return (
+    <Container className={styles.container}>
+        <div>
+            <img className={styles.girlImg} src="/images/shutterstock.png" alt="" />
+        </div>
+        <div className={styles.rocket}>
+            <div className={styles.rocketContainer}>
+            <img className={styles.rocketImg} src="/images/rocket.svg" alt="" />
+            </div>
+        </div>
+    </Container>
+  )
+} 
+export default Girl;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
 import { Contacts } from "..";
-import { Benefits, Eco, Smartcontacts, Socials, Story, Team } from "../../components";
+import { Benefits, Eco, Girl, Smartcontacts, Socials, Story, Team } from "../../components";
 import styles from ".//Main.module.scss";
 
 const Main = () => {
   return (
-    <div className={styles.girl}>
-      <img
-        className={styles.shutter}
-        src="/images/shutterstock.png"
-        alt="shutterstock"
-      />
-      <img src="/images/rocket.svg" alt="rocket" />
-      <img src="/images/rocketback.svg" alt="" />
+    <div>
+      <Girl />
       <Story />
       <Benefits />
       <Eco />
