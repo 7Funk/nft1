b26bbf2e489268ef424c9fe6d689b06cf80745b7
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2022-03-06 18:16:07 +0500

    header burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,30 @@
 import styles from ".//Header.module.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ConnectModal } from "../ConnectModal";
 
 const Header = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isHeaderOpen, setIsHeaderOpen] = useState(false);
 
-  const [isOpenModal, setIsOpenModal] = useState(false)
+  useEffect(() => {
+    if (isHeaderOpen) {
+      document.body.style = 'overflow: hidden'
+    } else {
+      document.body.style = 'overflow: auto'
+    }
+  }, [isHeaderOpen])
+  
 
+  const headerClass = () => {
+    let cls = styles.header;
+    if (isHeaderOpen) {
+      cls += ' ' + styles.responsive;
+    }
+    return cls;
+  };
   return (
-    <header className={styles.header}>
+    <header className={headerClass()}>
       <Link to="/" className={styles.link}>
         <img src="/images/headerLogo.svg" alt="logo" />
       </Link>
@@ -39,8 +55,22 @@ const Header = () => {
       <a className={styles.link} href="#whitepaper">
         Whitepaper
       </a>
-      <button onClick={() => {setIsOpenModal(true)}} className={styles.button}>CONNECT WALLET {">"}</button>
-      <ConnectModal isOpen={isOpenModal} onClose={() => {setIsOpenModal(false)}} />
+      <button
+        onClick={() => {
+          setIsOpenModal(true);
+        }}
+        className={styles.button}
+      >
+        CONNECT WALLET <span className={styles.arrow}>{">"}</span>
+      </button>
+      <img onClick={() => {setIsHeaderOpen(true)}} className={styles.burger} src="/images/Burger.svg" alt="burger" />
+      <img onClick={() => {setIsHeaderOpen(false)}} className={styles.cross} src="/images/Close.svg" alt="cross" />
+      <ConnectModal
+        isOpen={isOpenModal}
+        onClose={() => {
+          setIsOpenModal(false);
+        }}
+      />
     </header>
   );
 };
